3130634e1260e428f21227b6ae6b950667a45a5b
MOMENRIZQ <MOMEN_2001_@HOTMAIL.COM>
2025-01-07 23:22:48 +0200

    bottom_splash

---
+++ lib/core/utils/app_images.dart
@@ -0,0 +1,21 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+  
+  /// Assets for imgAppIcon
+  /// assets/img/app_icon.png
+  static const String imgAppIcon = "assets/img/app_icon.png";
+
+  /// Assets for imgBottomSplash
+  /// assets/img/bottom_splash.svg
+  static const String imgBottomSplash = "assets/img/bottom_splash.svg";
+
+  /// Assets for imgLogo
+  /// assets/img/logo.svg
+  static const String imgLogo = "assets/img/logo.svg";
+
+  /// Assets for imgPlanet
+  /// assets/img/planet.svg
+  static const String imgPlanet = "assets/img/planet.svg";
+}
+
